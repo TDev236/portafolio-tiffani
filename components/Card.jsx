d3780b6218dbe59b33efbfdd0e6c3e162afd04dd
king tiffani <tiffanirengifo23@gmail.com>
2022-05-26 19:06:11 -0500

    other-bug-fix

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,12 +4,12 @@ import Image from 'next/image'
 
 const Card = ({img, name, tecnology,url}) => {
   return (
-    <div className=" max-w-[350px]  m-8 transition ease-out delay-150 hover:scale-105">
+    <div className=" max-w-[350px]  m-8 md:transition ease-out delay-150 hover:scale-105">
         <div className="rounded-xl shadow-md shadow-naranja  p-2 hover:shadow-naranja-oscuro  ">
           <Image src={img} className=" rounded-xl"  width="400" height="300"  alt={name}/>
           <div className="flex flex-col items-end my-2 py-2 mx-4">
-            <h1 className="text-xl text-right px-4 text-texto font bold border-b w-[180px]  ">{name}</h1>
-            <p className="py-2 text-sm text-naranja-oscuro">{tecnology}</p>
+            <h1 className="text-xl  text-right px-4 text-texto font bold border-b w-[180px]  ">{name}</h1>
+            <p className="py-2 text-sm sm:text-xs md:text-lg text-naranja-oscuro">{tecnology}</p>
             <a target='_blank' rel="noreferrer" className="bg-naranja p-2 mt-2 rounded-lg text-texto transition ease-in-out delay-150 hover:translate-x-1" href={url}>Live Demo</a>
           </div>
         </div>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
   return (
     <div id='projects' className="flex flex-col justify-center items-center">
       <h1 className="mt-4 text-texto font-bold text-center text-4xl">This are some of my Projects</h1>
-      <div className='grid sm:grid-cols-3  gap-4'>
+      <div className='grid sm:grid-cols-2 md:grid-cols-3  gap-4'>
         {cardInfo.map(({name, tecnology, img, id, url}) => (
           <Card name={name} tecnology={tecnology} key={id} img={img} url={url}/>
         ))}
